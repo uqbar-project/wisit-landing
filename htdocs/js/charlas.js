5018e37ecd14c06015a424850406fb5c3c816737
Pablo Tesone <tesonep@gmail.com>
2014-11-17 17:17:17 -0300

    Agregar popup para charlas

---
+++ htdocs/js/charlas.js
@@ -0,0 +1,34 @@
+
+function showCharla(idCharla){
+	var titCharla = $("#charlas #" + idCharla + " h2").html();
+	var autorCharla = $("#charlas #" + idCharla + " h3").html();
+	var abstCharla = $("#charlas #" + idCharla + " div").html();
+	
+	$("#charlasPopup h2").html(titCharla);
+	$("#charlasPopup #autor").html(autorCharla);
+	$("#charlasPopup #abstract").html(abstCharla);
+	
+	var h = $(window).height() * 0.8;
+	var w = $(window).width() * 0.8;
+	
+	$("#charlasPopup").dialog({
+		  title: 'Detalle de la charla',
+		  modal: true,
+		  width: w,
+		  height: h,
+		  draggable:false,
+		  show: { effect: "blind", duration: 800 },
+		  buttons: [
+		    {
+		      text: "Cerrar",
+		      click: function() {
+		        $( this ).dialog( "close" );
+		      }
+		    }
+		  ]
+		});
+}
+
+$(document).ready(function(){
+	$("#charlas").load('data/charlas.html');
+});
