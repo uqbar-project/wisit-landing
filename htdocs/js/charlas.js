bbe82cc09312d10494d0422ebcd7da024b802ee2
Nicolás Passerini <npasserini@gmail.com>
2014-11-23 19:25:27 -0300

    Info detallada de charlas. Comenzé por la de Eric Pernia

--- htdocs/js/charlas.js
+++ htdocs/js/charlas.js
@@ -39,3 +39,41 @@ function showCharla(idCharla){
 $(document).ready(function(){
 	$("#charlas").load('data/charlas.html');
 });
+
+function showCharlaNew(idCharla){
+	var titCharla = $("#charlas #" + idCharla + " h2").html();
+	var contents = $("#charlas #" + idCharla + " div").html();
+	
+	$("#charlasPopupNew #contents").html(contents);
+	
+	var h = $(window).height() * 0.8;
+	var w = $(window).width() * 0.8;
+	
+	$("body").css("overflow","hidden");
+
+	$("#charlasPopupNew").dialog({
+		  title: titCharla,
+		  modal: true,
+		  width: w,
+		  height: h,
+		  position: { my: "center top", at: "center top+" + ($("header").height() + 10) , of: window },
+		  draggable:false,
+		  dialogClass: 'charlasDialog',
+		  show: { effect: "blind", duration: 800 },
+		  close: function(){
+		  	$("body").css("overflow","auto");
+		  },
+		  buttons: [
+		    {
+		      text: "Cerrar",
+		      click: function() {
+		        $( this ).dialog( "close" );
+		      }
+		    }
+		  ]
+		});
+}
+
+$(document).ready(function(){
+	$("#charlas").load('data/charlas.html');
+});
